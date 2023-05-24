ab8a61ca32d84cb5a52cdfa4288abafeaa3062be
ZephirSD <siegfrieddallery@gmail.com>
2023-05-24 20:43:37 +0400

    Création du serveur ExpressJS avec connexion à la base MongoDB

--- server/app.js
+++ server/app.js
@@ -1,16 +1,27 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = 6000;
+require('dotenv').config()
+const routeRepas = require('./routes/routesRepas');
+const cors = require("cors");
+
+app.use(cors());
+try{
+  mongoose.connect(process.env.LIEN_MONGO, { useNewUrlParser: true})
+  .then(()=>{
+    app.listen(port);
+    console.log('MongoDB Connecté');
+  })
+}
+catch{
+  console.error();
+}
 
 app.use(express.json());
-
-mongoose.connect('mongodb://localhost:27017/', { useNewUrlParser: true})
-.then(()=>{
-  app.listen(port);
-  console.log('MongoDB Connecté');
-})
+app.use('/api/repas', routeRepas);
 
 app.get('/', function (req, res) {
   res.send('Hello World');
 })
+
