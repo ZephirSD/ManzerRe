d30e550e8c421d70c93bc9aade10d62241415e60
ZephirSD <siegfrieddallery@gmail.com>
2023-05-25 07:32:56 +0400

    Ajout du fetch pour les datas des repas

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import  './style/style.scss';
+import Accueil from './pages/Accueil';
+import './style/index.css';
+import axios from "axios";
 
 function App() {
+  const [repasGet, setrepasGet] = useState([])
+  const fetchRepas = async () => {
+    await axios.get('http://localhost:5000/api/repas')
+    .then(response => {
+        setrepasGet(response.data.result);
+    }).catch(err => console.error);
+  }
+  useEffect(() => {
+      fetchRepas()
+    }, [])
   return (
     <>
+      <Accueil listeRepas={repasGet}/>
     </>
   )
 }
