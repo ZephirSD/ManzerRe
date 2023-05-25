2a28aa49f1042e506fcd61597d5f20094ad175b9
ZephirSD <siegfrieddallery@gmail.com>
2023-05-25 07:33:36 +0400

    Insertion des éléments dynamique de la CarteRepas

--- client/src/composants/CardRepasAccueil.js
+++ client/src/composants/CardRepasAccueil.js
@@ -1,19 +1,27 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
-import repas from '../assets/carry_bichique.jpg'
+import repasImage from '../assets/carry_bichique.jpg'
 
-function CardRepasAccueil() {
+function CardRepasAccueil({repas}) {
   return (
     <>
         <div class="max-w-sm rounded-lg shadow dark:bg-gray-800 dark:border-gray-700" id="cardRepasA">
             <a href="#">
-                <img class="rounded-t-lg" src={repas} alt="" />
+                <img class="rounded-t-lg" src={repasImage} alt="" />
             </a>
             <div class="p-5">
                 <a href="#">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-white dark:text-white">Carry Bichique</h5>
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-white dark:text-white">{repas.nom_repas}</h5>
                 </a>
-                <p class="mb-3 font-normal text-white dark:text-gray-400">#poisson</p>
+                <p class="mb-3 font-normal text-white dark:text-gray-400">
+                    <div style={{display: 'flex'}}>
+                        {
+                            repas.tags.map((tag,index)=> (
+                                <div key={index} style={{padding: '0px 5px'}}>{tag}</div>
+                            ))
+                        }
+                    </div>
+                </p>
                 <div class="flex items-center space-x-4">
                     <div class="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
                         <svg class="absolute w-12 h-12 text-gray-300 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
