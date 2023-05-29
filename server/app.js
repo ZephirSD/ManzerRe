bf8700c951e36293a4cf5cb1bcd034b363534487
ZephirSD <siegfrieddallery@gmail.com>
2023-05-29 17:00:39 +0400

    Ajout des composants route des Users dans l'`app.js`

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 5000;
 require('dotenv').config()
 const routeRepas = require('./routes/routesRepas');
+const routeUser = require('./routes/routesUser');
 const cors = require("cors");
 
 app.use(cors());
@@ -11,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 try{
-  mongoose.connect(process.env.LIEN_MONGO, { useNewUrlParser: true})
+  mongoose.connect(process.env.LIEN_MONGO_HOME, { useNewUrlParser: true})
   .then(()=>{
     app.listen(port);
     console.log('MongoDB Connecté');
@@ -22,6 +23,7 @@ catch{
 }
 
 app.use('/api/repas', routeRepas);
+app.use('/api/user',routeUser);
 
 app.get('/', function (req, res) {
   res.send('Hello World');
