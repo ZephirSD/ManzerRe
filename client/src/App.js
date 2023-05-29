2d3a67943fcbed946d60b9cbcb33050b54a69f0f
ZephirSD <siegfrieddallery@gmail.com>
2023-05-29 16:56:38 +0400

    Ajout de `repasGet` pour la liste des repas

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import Formulaire from './pages/Formulaire';
 import RepasClient from './pages/RepasClient';
 
 function App() {
-  const [repasGet, setrepasGet] = useState([])
+  const [repasGet, setrepasGet] = useState([]);
   const fetchRepas = async () => {
     await axios.get(`${process.env.REACT_APP_URL_FETCH}/api/repas`)
     .then(response => {
