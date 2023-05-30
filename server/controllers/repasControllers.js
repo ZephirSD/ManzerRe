de762aa59cdfe4a8b5b9e8fb53f4e71dfb0ac8c4
ZephirSD <siegfrieddallery@gmail.com>
2023-05-30 15:04:34 +0400

    Ajout du controlleur pour avoir un repas séléctionné pour la modification & modification du controlleur de création de repas

--- server/controllers/repasControllers.js
+++ server/controllers/repasControllers.js
@@ -12,8 +12,23 @@ const getRepasOne = ((req, res) => {
         .catch(() => res.status(404).json({msg: 'Product not found'}))
 })
 
+const getRepasOneRepas = ((req, res) => {
+    Repas.find({ _id: req.params.repasID })
+        .then(result => res.status(200).json({ result }))
+        .catch(() => res.status(404).json({msg: 'Product not found'}))
+})
+
 const createRepas = ((req, res) => {
-    Repas.create(req.body)
+    Repas.create(
+        {
+            nom_repas: req.body.nom_repas,
+            tags: req.body.tags,
+            secteur: req.body.secteur,
+            id_createur: req.body.id_createur,
+            nom_image: req.body.nom_image,
+            // image_data: req.body.image_data,
+        }
+    )
         .then(result => res.status(200).json({ result }))
         .catch((error) => res.status(500).json({msg:  error }))
 })
@@ -33,6 +48,7 @@ const deleteRepas = ((req, res) => {
 module.exports = {
     getRepas,
     getRepasOne,
+    getRepasOneRepas,
     createRepas,
     updateRepas,
     deleteRepas
