d596ee761617475ccda706978da77ca0f88c80de
ZephirSD <siegfrieddallery@gmail.com>
2023-05-30 15:02:58 +0400

    Suppression du bouton Création

--- client/src/pages/Accueil.js
+++ client/src/pages/Accueil.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import Header from '../composants/Header'
 import CardRepasAccueil from '../composants/CardRepasAccueil'
-import { Link } from 'react-router-dom';
 
 function Accueil({listeRepas}) {
     const [searchRepas, setsearchRepas] = useState("");
@@ -20,10 +19,7 @@ function Accueil({listeRepas}) {
             </div>
             <input onChange={(e) => setsearchRepas(e.target.value)} type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Rechercher" required/>
         </div>
-      </div>
-      <Link to={"/creer"}>
-        <button type="button" id='btnCreerRepas' class="text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 absolute right-0 mr-3 dark:focus:ring-blue-800">Créer repas</button>
-      </Link>
+      </div>      
     <ul class="items-center flex justify-center w-full text-sm font-medium text-gray-900 rounded-lg sm:flex dark:bg-gray-700 dark:text-white">
         <li class="w-80 dark:border-gray-600">
             <div class="flex items-center pl-3">
