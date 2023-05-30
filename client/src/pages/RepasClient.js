eba00bb0ec38f871fd66964082689456d2d3d63d
ZephirSD <siegfrieddallery@gmail.com>
2023-05-30 15:02:30 +0400

    Suppression du bouton de Création

--- client/src/pages/RepasClient.js
+++ client/src/pages/RepasClient.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Header from '../composants/Header'
-import { Link, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import axios from 'axios'
 import CardRepasClient from '../composants/CardRepasClient'
 
@@ -14,15 +14,12 @@ function RepasClient(){
         }).catch(err => console.error);
     }
     useEffect(() => {
-        fetchRepasClient()
+        fetchRepasClient();
     })
     
   return (
     <>
         <Header/>
-        <Link to={"/creer"}>
-            <button style={{margin: '2vh 15px 2vh 0px'}} type="button" id='btnCreerRepas' class="text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 absolute right-0 mr-3 dark:focus:ring-blue-800">Créer repas</button>
-        </Link>
         <h1 id="titreRepasClient">Vos Repas</h1>
         <div id='gridCardRepasAccueil' class="container m-auto">
         {
