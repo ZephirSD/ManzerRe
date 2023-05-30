012b38e7077b73cc03a85769bedecb87e6968b73
ZephirSD <siegfrieddallery@gmail.com>
2023-05-30 15:05:08 +0400

    Ajout du nom d'image pour le repas

--- server/models/RepasModels.js
+++ server/models/RepasModels.js
@@ -4,7 +4,9 @@ const RepasSchema = new mongoose.Schema({
     nom_repas: String,
     tags: Array,
     secteur: String,
-    id_createur: String
+    id_createur: String,
+    nom_image: String,
+    // image_data: Buffer
 })
 
 const Repas = mongoose.model('Repas', RepasSchema)
